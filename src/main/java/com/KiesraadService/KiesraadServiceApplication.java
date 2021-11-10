1497be2d6a0575946ddf0ad3a7e521b1229e7f29
Francois <francois.rombouts97@gmail.com>
2021-11-10 10:14:27 +0100

    Extracted wrong initializr

--- src/main/java/com/KiesraadService/KiesraadServiceApplication.java
+++ src/main/java/com/KiesraadService/KiesraadServiceApplication.java
@@ -1,13 +1,13 @@
-package com.APIGateway;
+package com.KiesraadService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class KiesraadServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(KiesraadServiceApplication.class, args);
 	}
 
 }
