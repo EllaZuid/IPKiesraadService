b5ad290c82e18ad8f9ed3e311825588c4dc335c6
Francois <francois.rombouts97@gmail.com>
2021-11-10 10:42:19 +0100

    Basic configurations

--- src/main/java/com/KiesraadService/KiesraadServiceApplication.java
+++ src/main/java/com/KiesraadService/KiesraadServiceApplication.java
@@ -2,8 +2,12 @@ package com.KiesraadService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableDiscoveryClient
 public class KiesraadServiceApplication {
 
 	public static void main(String[] args) {
