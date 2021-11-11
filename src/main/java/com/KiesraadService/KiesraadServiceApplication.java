d3c8c983af9a6e9982cc9066e76a3ce7222d1656
Francois <francois.rombouts97@gmail.com>
2021-11-11 09:11:44 +0100

    CORS error fix
    Probably fixes CORS error

--- src/main/java/com/KiesraadService/KiesraadServiceApplication.java
+++ src/main/java/com/KiesraadService/KiesraadServiceApplication.java
@@ -3,7 +3,10 @@ package com.KiesraadService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,6 +15,16 @@ public class KiesraadServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KiesraadServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("");
+			}
+		};
 	}
 
 }
--- src/main/java/com/KiesraadService/controller/MainController.java
+++ src/main/java/com/KiesraadService/controller/MainController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/council")
+@CrossOrigin(origins = "*")
 public class MainController {
 
     final PartyRepository partyRepository;
